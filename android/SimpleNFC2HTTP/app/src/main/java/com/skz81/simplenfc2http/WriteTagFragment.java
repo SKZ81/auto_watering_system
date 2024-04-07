444e58f3fcec5241b7be8e9ae8ee60172900aada
Johann CAHIER <johann.cahier@free.fr>
2024-04-07 14:57:17 +0200

    SimpleNFC2HTTP: Improve 'write' tab

--- android/SimpleNFC2HTTP/app/src/main/java/com/skz81/simplenfc2http/WriteTagFragment.java
+++ android/SimpleNFC2HTTP/app/src/main/java/com/skz81/simplenfc2http/WriteTagFragment.java
@@ -1,7 +1,12 @@
 package com.skz81.simplenfc2http;
 
-import android.os.Bundle;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.app.DatePickerDialog;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,11 +20,103 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
+import java.io.IOException;
 import java.util.UUID;
 import java.util.Calendar;
 
+import android.nfc.tech.Ndef;
+import android.nfc.NdefRecord;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+
+import com.skz81.simplenfc2http.MainActivity;
+
+
 public class WriteTagFragment extends Fragment {
+    protected class NdefWriteListener implements NdefTagCallback {
+        private String appName = null;
+        private WriteTagFragment parent;
+
+        public NdefWriteListener(WriteTagFragment fragment) {
+            parent = fragment;
+            try {
+                Context context = parent.getContext();
+                PackageManager packageManager = context.getPackageManager();
+                ApplicationInfo applicationInfo = packageManager.getApplicationInfo(context.getPackageName(), 0);
+                appName = (String) packageManager.getApplicationLabel(applicationInfo);
+            } catch (PackageManager.NameNotFoundException e) {
+                appName = "NOT_FOUND";
+            }
+        }
+        @Override
+        public void onNDEFDiscovered(Ndef ndef) {
+            NdefMessage message = new NdefMessage(
+                    NdefRecord.createTextRecord("", parent.getNewUUID()),
+                    NdefRecord.createTextRecord("", appName)
+            );
+            try {
+                ndef.connect();
+                if (ndef.isConnected()) {
+                    ndef.writeNdefMessage(message);
+                }
+                // check we're still connected
+                if (!ndef.isConnected()) {
+                    throw new IOException("NFC connection lost");
+                }
+
+                // update_server()...
+
+            } catch (Exception e) {
+                // Log error message
+                Log.e(TAG, "Error writing NDEF messages: " + e.getMessage(), e);
+                Toast.makeText(mainActivity, "Error writing tag...", Toast.LENGTH_LONG).show();
+            } finally {
+                newUUID = null;
+                try {
+                    ndef.close();
+                } catch (Exception e) {
+                    Log.w(TAG, "Error closing NDEF connection: " + e.getMessage(), e);
+                }
+            }
+            if (scanTagDialog != null) {
+                scanTagDialog.dismiss();
+                scanTagDialog = null;
+            }
+            parent.stopScan();
+        }
+    }
+
+    protected class NdefReadListener implements NdefTagCallback {
+        private WriteTagFragment parent;
+        public NdefReadListener(WriteTagFragment fragment) {
+            parent = fragment;
+        }
+        @Override
+        public void onNDEFDiscovered(Ndef ndef) {
+            NdefMessage message = ndef.getCachedNdefMessage();
+            NdefRecord[] records = message.getRecords();
+            for (int j = 0; j < records.length; j++) {
+                NdefRecord record = records[j];
+                Log.i(TAG, "  Record " + (j + 1) + ":");
+                Log.i(TAG, "    TNF (Type Name Format): " + record.getTnf());
+                Log.i(TAG, "    Type: " + new String(record.getType()));
+                Log.i(TAG, "    Payload: " + new String(record.getPayload()));
+            }
+            try {
+                ndef.close();
+            } catch (Exception e) {
+                Log.w(TAG, "Error closing NDEF connection: " + e.getMessage(), e);
+            }
+            if (scanTagDialog != null) {
+                scanTagDialog.dismiss();
+                scanTagDialog = null;
+            }
+            parent.stopScan();
+        }
+    }
+
     private static final String TAG = "AutoWatS-NFC-write";
 
     private EditText plantId;
@@ -27,13 +124,20 @@ public class WriteTagFragment extends Fragment {
     private EditText germinationDateEdit;
     private EditText bloomingDateEdit;
     private EditText yieldingDateEdit;
-    private Button generateIdButton;
-    private Button writeTagButton;
+    private Button updateInfoButton;
+    private Button scanTagButton;
+    private Button newPlantButton;
 
+    private MainActivity mainActivity;
     private Calendar calendar;
+    private AlertDialog scanTagDialog = null;
+
+    private String newUUID = null;
+
+    private NdefReadListener ndefReader;
+    private NdefWriteListener ndefWriter;
 
     public WriteTagFragment() {
-        // Required empty public constructor
     }
 
     private void addDateFieldListener(EditText textview) {
@@ -55,14 +159,19 @@ public class WriteTagFragment extends Fragment {
         germinationDateEdit = view.findViewById(R.id.germinationDateEdit);
         bloomingDateEdit = view.findViewById(R.id.bloomingDateEdit);
         yieldingDateEdit = view.findViewById(R.id.yieldingDateEdit);
-        generateIdButton = view.findViewById(R.id.generateIdButton);
-        writeTagButton = view.findViewById(R.id.writeTagButton);
+        updateInfoButton = view.findViewById(R.id.updateInfoButton);
+        scanTagButton = view.findViewById(R.id.scanTagButton);
+        newPlantButton = view.findViewById(R.id.newPlantButton);
 
         calendar = Calendar.getInstance();
+        mainActivity = (MainActivity) getActivity();
 
         addDateFieldListener(germinationDateEdit);
         addDateFieldListener(bloomingDateEdit);
         addDateFieldListener(yieldingDateEdit);
+
+        ndefReader = new NdefReadListener(this);
+        ndefWriter = new NdefWriteListener(this);
 
         // Populate variety spinner with dummy values
         // TODO : use the HTTP to get the list with images (and info URL)
@@ -78,25 +187,81 @@ public class WriteTagFragment extends Fragment {
         varietySpinner.setAdapter(adapter);
 
         // Generate ID button click listener
-        generateIdButton.setOnClickListener(new View.OnClickListener() {
+        newPlantButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Generate and set new UUID
-                String newUuid = UUID.randomUUID().toString();
-                plantId.setText(newUuid);
+                buttonNewPlantClicked();
             }
         });
 
         // Write Tag button click listener
-        writeTagButton.setOnClickListener(new View.OnClickListener() {
+        scanTagButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Implement functionality to write tag here
-                Toast.makeText(requireContext(), "Write Tag button clicked", Toast.LENGTH_SHORT).show();
+                buttonScanTagClicked();
+            }
+        });
+
+        // Write Tag button click listener
+        updateInfoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttoUpdateInfoClicked();
             }
         });
 
         return view;
+    }
+
+    void buttonNewPlantClicked() {
+        newUUID = UUID.randomUUID().toString();
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Format Tag")
+                .setMessage("This will format the tag, with\nnew UUID: " + newUUID
+                            + ".\nPlease scan the tag to proceed...")
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        if (mainActivity != null) {
+                            mainActivity.stopNFCScan();
+                        }
+                        dialog.dismiss();
+                        scanTagDialog = null;
+                    }
+                });
+        if (mainActivity != null) {
+            mainActivity.startNFCScan(ndefWriter);
+        }
+        scanTagDialog = builder.create();
+        scanTagDialog.show();
+    }
+
+    void buttonScanTagClicked() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Scan Tag")
+                .setMessage("Please scan the tag to display data...")
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        if (mainActivity != null) {
+                            mainActivity.stopNFCScan();
+                        }
+                        dialog.dismiss();
+                        scanTagDialog = null;
+                    }
+                });
+        if (mainActivity != null) {
+            mainActivity.startNFCScan(ndefReader);
+        }
+        scanTagDialog = builder.create();
+        scanTagDialog.show();
+    }
+
+    void buttoUpdateInfoClicked() {
+    }
+
+    public void stopScan() {
+        if (mainActivity != null) {
+            mainActivity.stopNFCScan();
+        }
     }
 
     public void showDatePickerDialog(EditText v) {
@@ -123,4 +288,7 @@ public class WriteTagFragment extends Fragment {
         datePickerDialog.show();
     }
 
+    public String getNewUUID() {
+        return newUUID;
+    }
 }
